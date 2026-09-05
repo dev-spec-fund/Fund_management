811e7bf07e05d3a3bdd1c4dd8cb5372c336081a7
dev-spec-fund <abdho.jameel.96@gmail.com>
2026-09-05 21:15:06 +0500

    Delete worker/migrations/0032_election_application_notifications.sql

--- worker/migrations/0032_election_application_notifications.sql
+++
@@ -1,6 +0,0 @@
--- Candidate application Telegram notification tracking.
-ALTER TABLE elections ADD COLUMN applications_notified_at TEXT;
-ALTER TABLE elections ADD COLUMN applications_reminder_at TEXT;
-
-INSERT OR IGNORE INTO schema_migrations(version,name)
-VALUES(32,'election_application_notifications');
